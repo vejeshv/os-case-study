e539cb6ae16888236569b5912f67a17c51bd6976
Arvind S Raj <sraj.arvind@gmail.com>
2013-11-11 15:48:18 +0530

    Added endpoint for API root

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,6 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.api_root),
     url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
