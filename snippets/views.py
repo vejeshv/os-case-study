b3801449136c537f41467276a78aea171a1c62b2
Arvind S Raj <sraj.arvind@gmail.com>
2013-11-11 15:30:29 +0530

    Added snippet views

--- snippets/views.py
+++ snippets/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from rest_framework import generics
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
