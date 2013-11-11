4ec636b31fb5bb6925b4ac0ea32b839e3a074e60
Arvind S Raj <sraj.arvind@gmail.com>
2013-11-11 18:24:01 +0530

    Added api-auth url

--- aclwebservice/urls.py
+++ aclwebservice/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^', include('snippets.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
