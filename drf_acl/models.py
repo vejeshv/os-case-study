2dcecb29d9d9925f126d741daffc9168205609e5
Arvind S Raj <sraj.arvind@gmail.com>
2013-11-11 18:27:13 +0530

    Added user permission table

--- drf_acl/models.py
+++ drf_acl/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from snippets.models import Snippet
+
+
+class SnippetUserPermission(models.Model):
+    """
+    Per user permissions for snippets
+    """
+
+    snippet = models.ForeignKey(Snippet)
+    user = models.ForeignKey(User)
+    get_perm = models.BooleanField(default=False)
+    post_perm = models.BooleanField(default=False)
+    delete_perm = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        Hack to fake a composite primary key
+        """
+        unique_together = (('snippet', 'user'),)
